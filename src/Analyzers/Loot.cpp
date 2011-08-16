5ba7e7a1d79b060ee5f1578baed2fe025f170e26
Stefan O'Rear <stefanor@cox.net>
2011-08-15 23:41:18 -0700

    Remove pre-rewrite item pickup and drop code from Loot

--- src/Analyzers/Loot.cpp
+++ src/Analyzers/Loot.cpp
@@ -32,7 +32,6 @@ using namespace std;
 Loot::Loot() : Analyzer("Loot"), _visit() {
 	/* register events */
 	EventBus::registerEvent(StashChanged::ID, this);
-	EventBus::registerEvent(ItemsOnGround::ID, this);
 }
 
 /* methods */
@@ -43,7 +42,9 @@ void Loot::analyze() {
 		return;
 	}
 
-	partitionFloorInventory();
+	// TODO: proper shopping code
+	if (World::level().tile().symbol() != SHOP_TILE && action::Loot::canLoot())
+		partitionFloorInventory();
 
 	/* don't move around when blind/confused/stunned/hallucinating */
 	if (Saiph::blind() || Saiph::confused() || Saiph::stunned() || Saiph::hallucinating())
@@ -73,39 +74,6 @@ void Loot::analyze() {
 		}
 		++v;
 	}
-
-	if (World::level().tile().symbol() == STAIRS_UP) {
-		/* stairs up, ask if anyone wants to drop anything and if they do, get down 3 E's and drop stuff */
-		WantItems wi(true, true);
-		for (map<unsigned char, Item>::iterator i = Inventory::items().begin(); i != Inventory::items().end(); ++i) {
-			/* only add items not in a slot */
-			if (Inventory::slotForKey(i->first) == ILLEGAL_SLOT)
-				wi.addItem(i->first, i->second);
-		}
-		bool drop_on_stairs = false;
-		EventBus::broadcast(&wi);
-		for (map<unsigned char, Item>::iterator i = wi.items().begin(); !drop_on_stairs && i != wi.items().end(); ++i) {
-			if (i->second.want() <= 0 || i->second.want() < i->second.count())
-				drop_on_stairs = true;
-		}
-		if (drop_on_stairs) {
-			ElberethQuery eq;
-			EventBus::broadcast(&eq);
-			if (eq.type() == ELBERETH_MUST_CHECK) {
-				/* we don't know, we must look */
-				World::setAction(new action::Look(this));
-			} else if (eq.type() == ELBERETH_DUSTED || eq.type() == ELBERETH_NONE) {
-				/* no elbereth or dusted elbereth */
-				if (eq.count() < 3) {
-					/* less than 3 elbereths, engrave */
-					World::setAction(new action::Engrave(this, ELBERETH "\n", HANDS, PRIORITY_LOOT_DROP, (eq.count() > 0)));
-				} else {
-					/* 3 or more elbereths, drop stuff we don't want */
-					World::setAction(new action::Drop(this, PRIORITY_LOOT_DROP, true));
-				}
-			}
-		}
-	}
 }
 
 void Loot::parseMessages(const string& messages) {
@@ -120,37 +88,6 @@ void Loot::onEvent(Event* const event) {
 		/* stash changed, we need to visit it again */
 		StashChanged* e = static_cast<StashChanged*> (event);
 		_visit.insert(e->position());
-	} else if (event->id() == ItemsOnGround::ID) {
-		/* standing on stash, ask if anyone want anything */
-		// TODO: proper shopping code
-		if (World::level().tile().symbol() != SHOP_TILE && action::Loot::canLoot()) {
-			ItemsOnGround* e = static_cast<ItemsOnGround*> (event);
-			int index = 0;
-			bool looting = false;
-			list<Item>::const_iterator i = e->items().begin();
-			/* if we're standing on STAIRS_UP we'll pretend it's a safe stash */
-			bool safe_stash = World::level().tile().symbol() == STAIRS_UP;
-			WantItems wi(false, safe_stash);
-			while (!looting) {
-				wi.addItem(index++, *i);
-				++i;
-				if (index == UCHAR_MAX || i == e->items().end()) {
-					EventBus::broadcast(&wi);
-					for (map<unsigned char, Item>::iterator i = wi.items().begin(); i != wi.items().end(); ++i) {
-						if (i->second.want() <= 0 || i->second.count() <= 0)
-							continue;
-						/* someone want an item in this stash */
-						World::setAction(new action::Loot(this, PRIORITY_LOOT, safe_stash));
-						looting = true;
-						break;
-					}
-					wi.clear();
-					index = 0;
-				}
-				if (i == e->items().end())
-					break;
-			}
-		}
 	}
 }
 
